d25f78c7e06894f1eb9873a769906867bbc81832
suraj0119 <surajkamble1319@gmail.com>
2024-06-13 22:51:52 +0530

    render-deploy

--- server.js
+++ server.js
@@ -1,11 +1,30 @@
-const express = require('express')
-const app = express()
+// const express = require('express')
+// const app = express()
+// const port = process.env.PORT || 4000;
+
+// app.get('/', (req, res) => {
+//   res.send("")
+// })
+
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`)
+// })
+
+const express = require('express');
+const fs = require('fs');
+const app = express();
 const port = process.env.PORT || 4000;
 
 app.get('/', (req, res) => {
-  res.send('db.json')
-})
+  fs.readFile('db.json', 'utf8', (err, data) => {
+    if (err) {
+      res.status(500).send('Error reading data');
+    } else {
+      res.send(data);
+    }
+  });
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
