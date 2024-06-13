ce1aa2d7dbeeef4a317a4b349156d5126c52963c
suraj0119 <surajkamble1319@gmail.com>
2024-06-14 01:32:25 +0530

    render-deploy

--- server.js
+++ server.js
@@ -10,21 +10,83 @@
 //   console.log(`Example app listening on port ${port}`)
 // })
 
+// const express = require('express');
+// const fs = require('fs');
+// const cors = require('cors'); // Import the cors middleware
+// const app = express();
+// const port = process.env.PORT || 4000;
+
+// // Use the cors middleware
+// app.use(cors());
+
+// app.get('/', (req, res) => {
+//   fs.readFile('db.json', 'utf8', (err, data) => {
+//     if (err) {
+//       res.status(500).send('Error reading data');
+//     } else {
+//       res.send(data);
+//     }
+//   });
+// });
+
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`);
+// });
+
+
 const express = require('express');
 const fs = require('fs');
-const cors = require('cors'); // Import the cors middleware
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
 const app = express();
 const port = process.env.PORT || 4000;
 
-// Use the cors middleware
 app.use(cors());
+app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
+// Get all songs
+app.get('/songs', (req, res) => {
   fs.readFile('db.json', 'utf8', (err, data) => {
     if (err) {
       res.status(500).send('Error reading data');
     } else {
-      res.send(data);
+      const db = JSON.parse(data);
+      res.json(db.songs);
+    }
+  });
+});
+
+// Get all users
+app.get('/users', (req, res) => {
+  fs.readFile('db.json', 'utf8', (err, data) => {
+    if (err) {
+      res.status(500).send('Error reading data');
+    } else {
+      const db = JSON.parse(data);
+      res.json(db.users);
+    }
+  });
+});
+
+// Add a new user
+app.post('/users', (req, res) => {
+  fs.readFile('db.json', 'utf8', (err, data) => {
+    if (err) {
+      res.status(500).send('Error reading data');
+    } else {
+      const db = JSON.parse(data);
+      const newUser = req.body;
+      newUser.id = db.users.length ? db.users[db.users.length - 1].id + 1 : 1;
+      db.users.push(newUser);
+
+      fs.writeFile('db.json', JSON.stringify(db, null, 2), (err) => {
+        if (err) {
+          res.status(500).send('Error writing data');
+        } else {
+          res.status(201).json(newUser);
+        }
+      });
     }
   });
 });
